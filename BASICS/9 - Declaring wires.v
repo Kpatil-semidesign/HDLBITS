1f6c84607b946d175d692b5cf9ae6e32f322e0f4
Kpatil-semidesign <semidv24@gmail.com>
2024-10-29 17:49:50 -0400

    Update 9 - Declaring wires.v

--- BASICS/9 - Declaring wires.v
+++ BASICS/9 - Declaring wires.v
@@ -1,6 +1,3 @@
-<img width="540" alt="Screenshot 2024-10-29 173634" src="https://github.com/user-attachments/assets/f1a3576a-be67-48e8-96e5-e03835ccc9f5">
-
-
 `default_nettype none
 module top_module(
     input a,
